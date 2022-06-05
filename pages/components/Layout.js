e79375e9b60a2bc2a4dd1dca3906a79f338efcde
Anderson Kardek <kardek.anderson@gmail.com>
2022-06-05 14:23:29 -0400

    add: criando layout, componentes principais e arquivos css

---
+++ pages/components/Layout.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+
+
+export default function Layout({children}) {
+  return(
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <title>PokeNext</title>
+      </Head>
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
